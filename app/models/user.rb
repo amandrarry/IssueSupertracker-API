319c5b502feb48b228b2857b4ecb35c21a490bb7
Sara <theredwolfblake66@hotmail.com>
2019-12-05 03:24:53 +0100

    ups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ApplicationRecord
-def self.from_omniauth(auth)
-    # Creates a new user only if it doesn't exist
-    where(email: auth.info.email).first_or_initialize do |user|
-      user.name = auth.info.name
-      user.email = auth.info.email
-    end
-end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2] 
+
+
+
+    def self.from_omniauth(auth)
+    	where(uid: auth.uid, provider: auth.provider).first_or_create do |user|
+	    user.email = auth.info.email
+	    user.password = Devise.friendly_token[0, 20]
+	    user.name = auth.info.name   # assuming the user model has a name
+		
+	    # If you are using confirmable and the provider(s) you use validate emails, 
+	    # uncomment the line below to skip the confirmation emails.
+	    # user.skip_confirmation!
+	  	end
+	end
 end
